3c33add439fa34d3e4a6b7fab8488aab6b8aed4a
stevethedeveloper <info@stevethedeveloper.com>
2023-11-28 17:01:19 -0800

    Add rough text field with delayed execution after user stops typing.

---
+++ SearchFlickr/Extensions+Utulities/DelayTextField.swift
@@ -0,0 +1,12 @@
+//
+//  DelayTextField.swift
+//  SearchFlickr
+//
+//  Created by Stephen Walton on 11/28/23.
+//
+
+import UIKit
+
+class DelayTextField: UITextField {
+
+}
---
+++ SearchFlickr/Extensions+Utulities/UITextField+Ext.swift
@@ -0,0 +1,29 @@
+//
+//  UITextField+Ext.swift
+//  SearchFlickr
+//
+//  Created by Stephen Walton on 11/28/23.
+//
+
+import UIKit
+
+class UITextField_Ext: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
