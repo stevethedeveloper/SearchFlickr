98ed6525b7622b033cdc753851f1b84a6072d5e1
stevethedeveloper <info@stevethedeveloper.com>
2023-11-28 16:49:16 -0800

    Set up programmatic structure.

---
+++ SearchFlickr/Controllers/SearchViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SearchViewController.swift
+//  SearchFlickr
+//
+//  Created by Stephen Walton on 11/28/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
